b9f3bafe09cf0d93fc18ee2612054225775c6b5e
Andrew Perotti <Perotti28@gmail.com>
2025-01-15 11:19:39 -0800

    Change data-book-number
    After testing with deleting existing books and then creating new books
    and looking at their data-book-number values I've realized that by
    using index values we are creating a problem, once we delete a book
    and then add a new book that new book might have the same
    data-book-number value as another book.
    If we create unique numbers that can be added as a property into the
    book objects and not rely on the array index values then we shouldn't
    have any problems.
    And sadly I'm going to use what I used earlier, a counter that
    increments each time a book is created, and since deleting a bookCard
    element does indeed cause a shift/repaint then we don't need to re
    render the entire group of bookCards as I had feared.
    Changed the delete buttons functionality to pull the identifier out and
    then use findIndex to get the array index of this specific book and
    then use splice to remove it from the array.
    I also removed an unnecessary use of bookCount in the  submitNewBook
    listener, it was not needed at all.

--- script.js
+++ script.js
@@ -5,37 +5,40 @@ let closeModal = document.querySelector('#closeModal')
 let submitNewBook = document.querySelector('#submitNewBook')
 
 const myLibrary = [];
+let bookCount = 0
 addBookToLibrary('To Kill a Mockingbird', "Harper Lee", 384, 'read')
 addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, 'read')
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, bookCount){
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
 	this.info = function(){
 		return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
-	}
+	},
+    this.bookNumber = bookCount
 }
 
 function addBookToLibrary(title, author, pages, read){
-    myLibrary.push(new Book(title, author, pages, read))
+    bookCount++
+    myLibrary.push(new Book(title, author, pages, read, bookCount))
 }
 
 
 function displayMyLibraryBooks(){
-    for(let i = 0; i<myLibrary.length; i++){
-        buildBookCard(myLibrary[i], i)
+    for(let book of myLibrary){
+        buildBookCard(book)
     }
 }
 displayMyLibraryBooks()
 
 
-function buildBookCard(book, index){
+function buildBookCard(book){
     let visualBook = document.createElement('div')
     visualBook.classList.add('book')
 
-    visualBook.setAttribute('data-book-number', index)
+    visualBook.setAttribute('data-book-number', book.bookNumber)
 
     let titleElement = document.createElement('h2')
     titleElement.textContent = 'Title: ' +book.title
@@ -58,9 +61,9 @@ function buildBookCard(book, index){
     deleteButton.classList.add('deleteButton')
     deleteButton.addEventListener('click', e=>{
         e.preventDefault();
-        /*check this value after deleting randoms */
-        let bookLibraryIndex = e.target.parentElement.getAttribute('data-book-number')
-        myLibrary.splice(bookLibraryIndex, 1)
+        let uniqueBookIdentifier = e.target.parentElement.getAttribute('data-book-number')
+        let bookPosition = myLibrary.findIndex(book=>book.bookNumber===uniqueBookIdentifier)
+        myLibrary.splice(bookPosition, 1)
         e.target.parentElement.remove()
     })
     visualBook.appendChild(deleteButton)
@@ -84,5 +87,6 @@ submitNewBook.addEventListener('click', e=>{
     let newBookPages = document.querySelector('#newPages').value 
     let newBookReadStatus = document.querySelector('#newRead').checked===true ? 'read' : 'not read'
     addBookToLibrary(newBookTitle, newBookAuthor, newBookPages, newBookReadStatus)
-    buildBookCard(myLibrary.slice(-1)[0], myLibrary.length-1)
+    /*myLibrary.slice(-1) would result in an array with only one element, the last element, using [0] we are specifically passing to this function an object*/
+    buildBookCard(myLibrary.slice(-1)[0])
 })
